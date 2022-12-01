de19ff7d0739af6cb73045204456c1b2597486ae
Gikonyo-Otieno <gikonyootieno@gmail.com>
2022-12-01 17:12:52 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+ const destructivelyAppendCat = cats.push('Ralph')
+ const destructivelyPrependCat = cats.unshift('Bob')
+ const destructivelyRemoveLastCat = cats.pop()
+ const destructivelyRemoveFirstCat = cats.shift()
+ const appendCat = ['Broom', ...cats]
+ const prependCat = [...cats,'Arnold']
+ const removeLastCat = cats.slice(2)
+ const removeFirstCat = cats.splice(0,1)
