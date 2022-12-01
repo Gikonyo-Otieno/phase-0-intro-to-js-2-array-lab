e6a832d701ba25188280b311e5e3180aa97b9cba
Gikonyo-Otieno <gikonyootieno@gmail.com>
2022-12-01 18:19:22 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const cats = ["Milo", "Otis", "Garfield"]
 
- const destructivelyAppendCat = cats.push('Ralph')
- const destructivelyPrependCat = cats.unshift('Bob')
- const destructivelyRemoveLastCat = cats.pop()
- const destructivelyRemoveFirstCat = cats.shift()
- const appendCat = ['Broom', ...cats]
- const prependCat = [...cats,'Arnold']
- const removeLastCat = cats.slice(2)
- const removeFirstCat = cats.splice(0,1)
+ function destructivelyAppendCat(name){ 
+    cats.push('Ralph')
+ } 
+ destructivelyaAppendCat('Ralph');  
+
+ function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+ }
+destructivelyPrependCat('Bob');
+
+ function destructivelyRemoveLastCat() {
+    cats.pop()
+ }
+ destructivelyRemoveLastCat();
+
+ function destructivelyRemoveFirstCat(){
+    cats.shift()
+ } 
+ destructivelyRemoveFirstCat();
+
+ function appendCat (name) {
+    ['Broom', ...cats]
+ }
+ appendCat('Broom');
+
+ function prependCat (name) {
+    [...cats,'Arnold']
+ }
+ prependCat('Arnold');
+
+ function removeLastCat() {
+    cats.slice(2)
+ }
+ removeLastCat()
+ function removeFirstCat() {
+    cats.splice(0,1)
+ }
+ removeFirstCat();
